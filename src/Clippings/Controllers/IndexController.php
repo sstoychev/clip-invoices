7e91c2851b80a3c437bc6f4ba8814f62135949da
Stoycho Stoychev <registration@sstoychev.name>
2021-04-29 02:36:30 +0200

    Basic functionality

--- src/Clippings/Controllers/IndexController.php
+++ src/Clippings/Controllers/IndexController.php
@@ -16,6 +16,7 @@
 namespace App\Clippings\Controllers;
 
 use App\Application\Settings\SettingsInterface;
+use Slim\Psr7\Response;
 
 /**
  * Class to handle the index
@@ -64,5 +65,89 @@ class IndexController extends Base
      */
     public function indexPost($request, $response, $args)
     {
+        $uploadedFiles = $request->getUploadedFiles();
+        $uploadedFile = $uploadedFiles['fileToUpload'];
+        $output = 'ERROR';
+        if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
+            $fileContent =  array_map('str_getcsv', file($_FILES['fileToUpload']['tmp_name']));
+            $output = '<pre>'.print_r($fileContent, true).'</pre><br />';
+            $headers = $fileContent[0];
+            // validate the file
+            list($missingHeaders, $headerIndexes) = $this->validateHeaders($headers);
+            if ($missingHeaders) {
+                $output .= 'ERROR: missing columns:'.implode(',', $missingHeaders);
+            }
+            $missingParents = $this->validateParent($fileContent, $headerIndexes);
+            $missingCurrencies = $this->validateCurrencies($fileContent, $headerIndexes);
+            // check if we are good to go
+            if (!$missingHeaders && !$missingParents && !$missingCurrencies) {
+                $this->calcTotal($fileContent, $headerIndexes);
+            }
+        }
+        $response->getBody()->write($output);
+        return $response;
+    }
+    /**
+     * Validate if all required headers are present
+     *
+     * @param array $headers -
+     *
+     * @return array
+     */
+    private function validateHeaders($headers)
+    {
+        global $app;
+        $container = $app->getContainer();
+        $settings = $container->get(SettingsInterface::class);
+        $requiredHeaders = $settings->get('csv_headers');
+
+        $missing = [];
+        $headerIndexes = [];
+        foreach ($requiredHeaders as $header) {
+            $index = array_search($header, $headers);
+            if ($index === false) {
+                $missing[] = $header;
+            }
+        }
+        return ['missing' => $missing, 'headerIndexes' => $headerIndexes];
+    }
+    /**
+     * Validate if all parents are correct
+     *
+     * @param array $headers -
+     *
+     * @return array
+     */
+    private function validateParent($fileContent, $headerIndexes)
+    {
+        return []; // TODO(Stoycho)
+    }
+    /**
+     * Validate if all parents are correct
+     *
+     * @param array $headers -
+     *
+     * @return array
+     */
+    private function validateCurrencies($fileContent, $headerIndexes)
+    {
+        return []; // TODO(Stoycho)
+    }
+
+    private function calcTotal($fileContent, $headerIndexes)
+    {
+        $typeInvoice = 1; // constant
+        $typeCredit = 2; // constant
+        $typeDebit = 3; // constant
+        $typeColumn  = $headerIndexes['Type'];
+        $totalColumn = $headerIndexes['Total'];
+        $total = 0;
+        for ($i=1; $i<count($fileContent); $i++) {
+            if ($fileContent[$i][$typeColumn] == $typeInvoice) {
+                $total += (float) $fileContent[$i][$totalColumn];
+            }
+        }
+        $output = 'Total:'. $total;
+        return $output;
     }
 }
